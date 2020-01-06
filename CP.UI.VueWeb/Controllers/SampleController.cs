46518aec6fd9449698951346a841025e9c4b7c7b
zyx <zyx@acortinfo.com>
2020-01-06 18:35:03 +0800

    commit files

---
+++ CP.UI.VueWeb/Controllers/SampleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CP.UI.VueWeb.Controllers
+{
+    public class SampleController : Controller
+    {
+        private static string[] Characters = new[]
+        {
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"
+        };
+
+        public IActionResult Index()
+        {
+            return Json(Characters);
+        }
+    }
+}
